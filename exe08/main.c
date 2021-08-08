9a8f26dc0a97444383c0bcf4ab07034fe8017696
Virgílio Jorge <88002109+virgilio212@users.noreply.github.com>
2021-08-08 19:22:48 +0000

    exe08

--- exe08/main.c
+++ exe08/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+	
+		int i=0, k=0, j=0, m=0;
+		int vet[1000];
+		
+		for (k=0; m==0; k++) {
+			scanf("%i", &vet[k]);
+			j++; 
+			if (vet[k]==0) {
+				m++; }
+		}
+
+		for (i=0; i<(j-1); i++) {
+			if (vet[i]>0)
+				printf("\nPOSITIVO");
+			else if (vet[i]<0)
+				printf("\nNEGATIVO"); }
+		
 }
