2d8c24e82f048e4dd6946e1bf8d30047507bf09c
Virgílio Jorge <88002109+virgilio212@users.noreply.github.com>
2021-08-08 20:04:47 +0000

    exe02

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+	
+	int temp, veloc, rend, dist, litr;
+	
+	scanf("%i", &temp);
+	scanf("%i", &veloc);
+	scanf("%i", &rend);
+	
+	dist=temp*veloc;
+	litr=dist/rend;
+	
+	printf("R = %d", rend);
+	printf("\nV = %d", veloc);
+	printf("\nT = %d", temp);
+	printf("\nD = %d", dist);
+	printf("\nL = %d", litr);
 }
