1fef59ae0e6dce9b4dadd3b8fc8480cb8dcf341d
Virgílio Jorge <88002109+virgilio212@users.noreply.github.com>
2021-08-08 20:21:17 +0000

    exe04

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int n=3, maior, menor;
+   int k=0;
+   
+	int vet[n];
+	
+	for (k=0; k<n; k++) {
+			scanf("%i", &vet[k]);
+			if (k==0) {
+				maior=vet[k];
+				menor=vet[k]; }
+			if (vet[k]>maior)
+				maior=vet[k];
+			if (vet[k]<menor)
+				menor=vet[k]; }
+		
+	printf("MENOR = %d", menor);		
+	printf("\nMAIOR = %d", maior);
+	
    return 0;
 }
