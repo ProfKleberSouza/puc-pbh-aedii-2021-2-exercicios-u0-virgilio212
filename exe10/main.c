1b4ec20502ee16d1c9ea9899525848e7e2d3f4a5
Virgílio Jorge <88002109+virgilio212@users.noreply.github.com>
2021-08-09 00:07:45 +0000

    exe10

--- exe10/main.c
+++ exe10/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int inverter(int vet[]) {
+	int i=0, f=10-1;
+	
+	 while (i<f) {
+		 trocar(vet, i, f);
+		 i++; f--;
+	 } }
+
+	
+int trocar(int vet[], int i, int f) {
+	int aux=vet[i];
+	vet[i]=vet[f];
+	vet[f]=aux;
+}
+
+int main(){
+	int k;
+	
+	int vet[10];
+	
+		for (k=0; k<10; k++)
+			scanf("%i", &vet[k]);
+		
+inverter(vet);
+
+	for (k=0; k<10; k++)
+		printf("\n%d", vet[k]);
+		
 }
