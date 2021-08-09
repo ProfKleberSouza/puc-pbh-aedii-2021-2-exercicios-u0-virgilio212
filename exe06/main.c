a1cc80881a8286d8ad755a9aee3407fd84e51b15
Virgílio Jorge <88002109+virgilio212@users.noreply.github.com>
2021-08-09 01:31:09 +0000

    exe06

--- exe06/main.c
+++ exe06/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 int main() {
+	
+	int k;
+	int vet[3];
+	
+		for (k=0; k<3; k++)
+			scanf("%i", &vet[k]); 
 
-
-
-
-
-
+	if((vet[1]!=vet[2])&&(vet[1]!=vet[3])&&(vet[2]!=13))
+		printf("TRIANGULO ESCALENO");
+		
+	
+	else { if((vet[1]==vet[2])&&(vet[2]==vet[3]))
+		printf("TRIANGULO EQUILATERO");
+	
+	else
+		printf("TRIANGULO ISOSCELES"); }
+		
 }
